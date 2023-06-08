b5e1e3613080139b595709563f34bab7d888f6fb
Mark Boiko <boikomark555@gmail.com>
2023-06-08 18:39:23 +0300

    initial setup 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['@mate-academy/eslint-config-react-typescript', 'plugin:cypress/recommended'],
+  extends: ['@mate-academy/eslint-config-react-typescript'],
   rules: {
     'max-len': ['error', {
       ignoreTemplateLiterals: true,
