855ec1141ba31dee6cbaed1683a7a368fd946751
kozlo <kozlovilla@gmail.com>
2023-06-09 18:39:09 +0300

    correct index.tsx, delete cypress and correct routes

---
+++ src/components/PhonesPage/PhonesPage.tsx
@@ -0,0 +1,11 @@
+import { ProductList } from '../ProductList/ProductList';
+
+export const PhonesPage = () => {
+  return (
+    <>
+      <h1>Phones Page</h1>
+
+      <ProductList />
+    </>
+  );
+};
