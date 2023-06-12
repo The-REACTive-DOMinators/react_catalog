b1e993424cc559b5c185d2b1c1cef04fc4e6e529
Anna Kuzmenko <mogilevs.anna@gmail.com>
2023-06-12 16:33:40 +0300

    added phone card

--- src/App.tsx
+++ src/App.tsx
@@ -7,28 +7,10 @@ import { AccessoriesPage } from './modules/AccessoriesPage/AccessoriesPage';
 import { ShoppingCartPage } from './modules/ShoppingCartPage/ShoppingCartPage';
 import { FavoritesPage } from './modules/FavoritesPage/FavoritesPage';
 import { PageNotFound } from './modules/PageNotFound/PageNotFound';
-import { Device } from './types/Device';
-import { Card } from './components/Card';
-
-const phone: Device = {
-  id: '6',
-  category: 'phones',
-  phoneId: 'apple-iphone-7-32gb-black',
-  name: 'Nexus S',
-  fullPrice: 500,
-  price: 480,
-  screen: '4.0 inches',
-  capacity: '16384MB',
-  color: 'black',
-  ram: '512MB',
-  year: 2016,
-  image: 'img/phones/apple-iphone-7/black/00.jpg',
-};
 
 const App = () => (
   <div className="App">
     <h1>React Phone Catalog</h1>
-    <Card phone={phone} />
     <Routes>
       <Route path="/" element={<HomePage />}>
         <Route path="/home" element={<Navigate to="/" />} />
