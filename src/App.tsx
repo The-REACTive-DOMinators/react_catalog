3abec24dd444bd6078293154b58c33aeacc46a19
Anna Kuzmenko <mogilevs.anna@gmail.com>
2023-06-12 23:10:55 +0300

    added component button

--- src/App.tsx
+++ src/App.tsx
@@ -7,27 +7,9 @@ import { AccessoriesPage } from './modules/AccessoriesPage/AccessoriesPage';
 import { ShoppingCartPage } from './modules/ShoppingCartPage/ShoppingCartPage';
 import { FavoritesPage } from './modules/FavoritesPage/FavoritesPage';
 import { PageNotFound } from './modules/PageNotFound/PageNotFound';
-import { Card } from './components/Card';
-
-const phone = {
-  id: '1',
-  category: 'phones',
-  phoneId: 'apple-iphone-7-32gb-black',
-  itemId: 'apple-iphone-7-32gb-black',
-  name: 'Apple iPhone 7 32GB Black',
-  fullPrice: 400,
-  price: 375,
-  screen: '4.7 IPS',
-  capacity: '32GB',
-  color: 'black',
-  ram: '2GB',
-  year: 2016,
-  image: 'img/phones/apple-iphone-7/black/00.jpg',
-};
 
 const App = () => (
   <div className="App">
-    <Card phone={phone} />
     <h1>React Phone Catalog</h1>
     <Routes>
       <Route path="/" element={<HomePage />}>
