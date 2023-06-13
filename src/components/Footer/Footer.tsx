0d6f6361f9ba51216fcb20e326e1bd6688397545
Dima <123905459+DimaDvm@users.noreply.github.com>
2023-06-13 15:20:18 +0300

    add header and footer with new branch (#13)
    * add header and footer with new branch
    
    * fix header.scss space

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,58 @@
+import { FC } from 'react';
+import './styles/main.scss';
+import { Arrow } from '../../icons/Arrow';
+import { LogoIcon } from '../../icons/Logo';
+
+export const Footer: FC = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <footer className="footer">
+      <LogoIcon className="footer__logo" />
+
+      <nav className="footer__nav nav-footer">
+        <ul className="nav-footer__list">
+          <li className="nav-footer__item">
+            <a
+              href="!#"
+              className="nav-footer__link"
+              target="_blank"
+            >
+              Github
+            </a>
+          </li>
+
+          <li className="nav-footer__item">
+            <a
+              href="!#"
+              className="nav-footer__link"
+              target="_blank"
+            >
+              Contacts
+            </a>
+          </li>
+
+          <li className="nav-footer__item">
+            <a
+              href="!#"
+              className="nav-footer__link"
+              target="_blank"
+            >
+              Rights
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <button type="submit" onClick={scrollToTop} className="back-top">
+        Back to Top
+        <Arrow className="footer__arrow" />
+      </button>
+
+    </footer>
+  );
+};
