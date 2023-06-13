ff862a11d89466beff30c2bc46f2c4206530b33a
Dima <dmitriy.movchan.02@gmail.com>
2023-06-13 22:11:49 +0300

    create TechSpecs

---
+++ src/components/TechSpecs/Chars.ts
@@ -0,0 +1,10 @@
+export interface Chars {
+  'Screen': string,
+  'Resolution': string,
+  'Processor': string,
+  'RAM': string,
+  'Built in memory': string,
+  'Camera': string,
+  'Zoom': string,
+  'Cell': string,
+}
