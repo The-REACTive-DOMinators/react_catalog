ba640e34a57122e5261c549e78dc065072ec18ae
Anna Kuzmenko <mogilevs.anna@gmail.com>
2023-06-13 19:02:06 +0300

    Added button components

--- src/icons/FavouriteFullIcon.tsx
+++ src/icons/FavouriteFullIcon.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable max-len */
 import { FCIcon } from './iconsType';
 
-export const FavoriteFullIcon: FCIcon = () => (
-  <svg width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+export const FavoriteFullIcon: FCIcon = (props) => (
+  <svg width="16" height="14" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
 
     <path
       fillRule="evenodd"
