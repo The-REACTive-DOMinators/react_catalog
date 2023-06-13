3892c59305ae5000aec43fc506b221ebdde710d5
Oleksandr <stambulyak1012@gmail.com>
2023-06-13 19:30:28 +0300

    deleted delay func

--- src/services/fetchPhones.ts
+++ src/services/fetchPhones.ts
@@ -1,34 +1,23 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 const BASE_URL = 'https://server-store-p1t7.onrender.com';
 
-// returns a promise resolved after a given delay
-function wait(delay: number) {
-  return new Promise(resolve => {
-    setTimeout(resolve, delay);
-  });
-}
-
-// To have autocompletion and avoid mistypes
 type RequestMethod = 'GET' | 'POST' | 'PATCH' | 'DELETE';
 
 function request<T>(
   url: string,
   method: RequestMethod = 'GET',
-  data: any = null as any, // we can send any data to the server
+  data: any = null as any,
 ): Promise<T> {
   const options: RequestInit = { method };
 
   if (data) {
-    // We add body and Content-Type only for the requests with data
     options.body = JSON.stringify(data);
     options.headers = {
       'Content-Type': 'application/json; charset=UTF-8',
     };
   }
 
-  // we wait for testing purpose to see loaders
-  return wait(300)
-    .then(() => fetch(BASE_URL + url, options))
+  return fetch(BASE_URL + url, options)
     .then(response => {
       if (!response.ok) {
         throw new Error();
