37dfe05e75ec60924aad3548985edba3bab2acf0
Dima <dmitriy.movchan.02@gmail.com>
2023-06-14 19:53:48 +0300

    add tech spec logic and put TechSpecs component in description

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ import { BurgerMenu } from './components/BurgerMenu';
 
 const App = () => (
   <div className="App">
-    <h1>React Phone Catalog</h1>
     <Header />
     <Routes>
       <Route path="/" element={<HomePage />}>
