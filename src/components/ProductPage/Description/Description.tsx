2e1ca099886050e65926195ba4657b3fea2ea390
Denys Krysanov <denis.krisanov321@gmail.com>
2023-06-15 18:49:43 +0300

    loading info about product added (#31)

--- src/components/ProductPage/Description/Description.tsx
+++ src/components/ProductPage/Description/Description.tsx
@@ -1,18 +1,17 @@
 /* eslint-disable max-len */
 import { FC } from 'react';
 import { About } from '../About-section/About';
+import { Summary } from '../../../types/Summary';
 
-const objs = [
-  { title: 'And then there was Pro', text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium excepturi quo veritatis molestiae aliquam optio labore rem quasi? Facere provident itaque ut recusandae voluptatibus sunt laudantium corrupti illum inventore ipsum?' },
-  { title: 'Camera', text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime officiis beatae nobis quisquam tenetur, enim nesciunt dolorum omnis! Aliquam, voluptatibus velit optio nobis pariatur corporis tempora cum aperiam laborum ad incidunt quisquam illo tenetur maiores nesciunt sapiente error necessitatibus quibusdam.' },
-  { title: 'Shoot it.Flip it. Zoom it. Crop it. Cut it. Light it. Tweak it. Love it.', text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime officiis beatae nobis quisquam tenetur, enim nesciunt dolorum omnis! Aliquam, voluptatibus velit optio nobis pariatur corporis tempora cum aperiam laborum ad incidunt quisquam illo tenetur maiores nesciunt sapiente error necessitatibus quibusdam.' },
-];
+interface Props {
+  descriptionObj: Summary['description'];
+}
 
-export const Description: FC = () => {
+export const Description: FC<Props> = ({ descriptionObj }) => {
   return (
     <div className="grid">
       <div className="grid__item--tablet-1-12">
-        <About description={objs} />
+        <About description={descriptionObj} />
       </div>
     </div>
   );
--- src/components/ProductPage/Product.tsx
+++ src/components/ProductPage/Product.tsx
@@ -1,20 +1,46 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { Description } from './Description/Description';
 import { PhotosBlock } from './PhotosBlock/PhotosBlock';
+import { getPhoneDescription } from '../../api/phones';
+import { Summary } from '../../types/Summary';
 
 export const Product: FC = () => {
-  const images = [
-    'img/phones/apple-iphone-11/black/00.jpg',
-    'img/phones/apple-iphone-11/black/01.jpg',
-    'img/phones/apple-iphone-11/black/02.jpg',
-    'img/phones/apple-iphone-11/black/03.jpg',
-    'img/phones/apple-iphone-11/black/04.jpg',
-  ];
+  const [images, setImages] = useState(['']);
+  const [summary, setSummary] = useState<Summary['description']>([
+    {
+      title: '',
+      text: [''],
+    },
+  ]);
+  const { phoneId } = useParams();
+
+  async function getPhoneById(id: string) {
+    try {
+      const phoneDescription = await getPhoneDescription(id);
+
+      window.console.log(phoneDescription);
+      setImages(phoneDescription.images);
+      setSummary(phoneDescription.description);
+
+      return phoneDescription;
+    } catch (error) {
+      window.console.log(error);
+
+      return null;
+    }
+  }
+
+  useEffect(() => {
+    if (phoneId) {
+      getPhoneById(phoneId);
+    }
+  }, []);
 
   return (
     <>
       <PhotosBlock images={images} />
-      <Description />
+      <Description descriptionObj={summary} />
     </>
   );
 };
