790c32da98e00455d8432d4cd4ad92171f7be9fb
Marko Boiko <106397016+VenomMB@users.noreply.github.com>
2023-06-15 11:10:40 +0300

    add photos_block (#24)
    Co-authored-by: Mark Boiko <boikomark555@gmail.com>

--- src/components/ProductPage/Product.tsx
+++ src/components/ProductPage/Product.tsx
@@ -1,8 +1,20 @@
 import { FC } from 'react';
 import { Description } from './Description/Description';
+import { PhotosBlock } from './PhotosBlock/PhotosBlock';
 
 export const Product: FC = () => {
+  const images = [
+    'img/phones/apple-iphone-11/black/00.jpg',
+    'img/phones/apple-iphone-11/black/01.jpg',
+    'img/phones/apple-iphone-11/black/02.jpg',
+    'img/phones/apple-iphone-11/black/03.jpg',
+    'img/phones/apple-iphone-11/black/04.jpg',
+  ];
+
   return (
-    <Description />
+    <>
+      <PhotosBlock images={images} />
+      <Description />
+    </>
   );
 };
