147d332ab822a2bf64ab56913dd23ffe144de542
Oleksandr <stambulyak1012@gmail.com>
2023-06-15 15:53:13 +0300

    impemented full logic off loading phones and pagination

--- src/api/phones.ts
+++ src/api/phones.ts
@@ -3,7 +3,11 @@ import { PhoneDescription } from '../types/PhoneDescription';
 import { client } from '../services/fetchPhones';
 
 export const getPhones = () => {
-  return client.get<Device[]>('/phones');
+  return client.get<Device[]>('/products/phones');
+};
+
+export const getSortedPhones = (sortParams: string) => {
+  return client.get<Device[]>(`/products/phones?${sortParams}`);
 };
 
 export const getPhone = (phoneId: string) => {
