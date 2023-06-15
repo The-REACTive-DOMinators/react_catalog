f9ac2f0e61d0bda928d6816143be3ee7ff7236e7
kozlo <kozlovilla@gmail.com>
2023-06-15 17:57:57 +0300

    solve photos block problem

--- src/components/PhotosBlock/PhotosBlock.tsx
+++ src/components/PhotosBlock/PhotosBlock.tsx
@@ -5,39 +5,35 @@ interface Props {
 }
 
 export const PhotosBlock: React.FC<Props> = ({ images }) => {
-  const [selectedItem, setSelectedItem] = useState('');
+  const [selectedItem, setSelectedItem] = useState(images[0]);
 
   const BASE_URL = 'https://server-store-p1t7.onrender.com';
 
   return (
-    <>
-      <h1>Photo block</h1>
-
-      <div className="photo-block">
-        <div className="photo-block__list">
-          {images.map((image) => (
-            <button
-              className="photo-block__list__item"
-              type="button"
-              onClick={() => setSelectedItem(image)}
-            >
-              <img
-                src={`${BASE_URL}${image}`}
-                alt="phone"
-                className="photo-block__list__item__img"
-              />
-            </button>
-          ))}
-        </div>
-
-        <div className="photo-block__selectedItem">
-          <img
-            src={selectedItem}
-            alt="phone"
-            className="photo-block__list__item__img"
-          />
-        </div>
+    <div className="photo-block">
+      <div className="photo-block__list">
+        {images.map((image) => (
+          <button
+            className="photo-block__list__item"
+            type="button"
+            onClick={() => setSelectedItem(image)}
+          >
+            <img
+              src={`${BASE_URL}/${image}`}
+              alt="phone"
+              className="photo-block__list__item__img"
+            />
+          </button>
+        ))}
       </div>
-    </>
+
+      <div className="photo-block__selectedItem">
+        <img
+          src={`${BASE_URL}/${selectedItem}`}
+          alt="phone"
+          className="photo-block__list__item__img"
+        />
+      </div>
+    </div>
   );
 };
