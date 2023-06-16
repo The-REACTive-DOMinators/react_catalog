d647aa0f37b48bad68e03e6611ab74279fb0c0a6
Dima <dmitriy.movchan.02@gmail.com>
2023-06-16 18:02:10 +0300

    Create SpecMap

--- src/components/ProductPage/AddToCartSection/Chars.ts
+++
@@ -1,6 +0,0 @@
-export interface Chars {
-  'Screen': string,
-  'Resolution': string,
-  'Processor': string,
-  'RAM': string,
-}
--- src/components/ProductPage/TechSpecs/TechSpecs.tsx
+++ src/components/ProductPage/TechSpecs/TechSpecs.tsx
@@ -1,24 +1,17 @@
 import { FC } from 'react';
 import './styles/main.scss';
 import { DeviceSpecs } from '../../../types/DeviceSpecs';
+import { SpecMap } from '../SpecMap/SpecMap';
 
 interface Props {
   chars: DeviceSpecs;
 }
 
 export const TechSpecs: FC<Props> = ({ chars }) => {
-  const specNames = Object.keys(chars);
-
   return (
     <div className="container">
       <h2 className="main-title">Tech specs</h2>
-      {specNames.map((key) => (
-        <section className="char" key={key}>
-          <p className="name">{key}</p>
-
-          <p className="value">{chars[key as keyof DeviceSpecs]}</p>
-        </section>
-      ))}
+      <SpecMap chars={chars} />
     </div>
   );
 };
