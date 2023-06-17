205c95df0d531744b60323fc076480621102448c
Dima <dmitriy.movchan.02@gmail.com>
2023-06-17 19:16:36 +0300

    add Links, fix bugs in current section

--- src/components/HomePage/ShopByCategory/ShopByCategory.tsx
+++ src/components/HomePage/ShopByCategory/ShopByCategory.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import './styles/main.scss';
+import { Link } from 'react-router-dom';
 import Phones from './pictures/Phones.png';
 import Tablets from './pictures/Phones (1).png';
 import Accessories from './pictures/Phones (2).png';
@@ -9,19 +10,19 @@ export const ShopByCategory: FC = () => {
 
   const categories = [
     {
-      title: 'Mobile phones',
+      title: 'mobile phones',
       subtitle: '95 models',
       img: Phones,
       // img: `${BASE_URL}/img/category-phones.png`,
     },
     {
-      title: 'Tablets',
+      title: 'tablets',
       subtitle: '24 models',
       img: Tablets,
       // img: `${BASE_URL}/img/category-tablets.png`,
     },
     {
-      title: 'Accessories',
+      title: 'accessories',
       subtitle: '100 models',
       img: Accessories,
       // img: `${BASE_URL}/img/category-accessories.png`,
@@ -29,23 +30,33 @@ export const ShopByCategory: FC = () => {
   ];
 
   return (
-    <div className="section">
-      <h2 className="section-title">Shop by category</h2>
+    <div className="container">
+      <div className="section">
+        <h2 className="title">Shop by category</h2>
 
-      <div className="categories grid">
-        {categories.map(({ title, subtitle, img }, index) => (
-          <div className={`category grid__item--desktop-${24 - 8 * (categories.length - index)}`}>
-            <img
-              className="category-picture"
-              src={img}
-              alt={title}
-            />
+        <div className="categories grid">
+          {categories.map(({ title, subtitle, img }, index) => (
+            <Link to={`/${title.split(' ').length > 1 ? title.split(' ')[1] : title}`} key={title}>
+              <div className={
+                `category
+               grid__item--desktop-${24 - 8 * (categories.length - index)}
+              grid__item--tablet-${12 - 4 * (categories.length - index)}`
+              }
+              >
+                <div className="picture-container">
+                  <img
+                    className="category-picture"
+                    src={img}
+                    alt={title}
+                  />
+                </div>
+                <h3 className="category-title">{title}</h3>
 
-            <h3 className="category-title">{title}</h3>
-
-            <p className="category-subtitle">{subtitle}</p>
-          </div>
-        ))}
+                <p className="category-subtitle">{subtitle}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
