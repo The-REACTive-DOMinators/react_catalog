0732fb0149f507ea0cba8c135bbf09b56f8b2562
Dima <dmitriy.movchan.02@gmail.com>
2023-06-18 22:14:05 +0300

    Add pirctures to shopByCategories-section

--- src/components/HomePage/ShopByCategory/ShopByCategory.tsx
+++ src/components/HomePage/ShopByCategory/ShopByCategory.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import { FC } from 'react';
 import './styles/main.scss';
 import { Link } from 'react-router-dom';
@@ -9,69 +10,69 @@ export const ShopByCategory: FC = () => {
         <h2 className="title-categories">Shop by category</h2>
 
         <div className="categories grid">
-          <Link to="/phones">
-            <div
-              className="
-                category-categories_section
-                grid__item--desktop-1-8
-                grid__item--tablet-1-4
-                grid__item-1-4
-              "
-            >
-              <div className="picture-container-categories">
-                <img
-                  className="category-categories_section-picture"
-                  alt="tablets"
-                />
-              </div>
-              <h3 className="category-categories_section-title">tablets</h3>
-
-              <p className="category-categories_section-subtitle">24 models</p>
+          <Link
+            to="/phones"
+            className="
+              category-categories_section
+              grid__item--desktop-1-8
+              grid__item--tablet-1-4
+              grid__item-1-4
+            "
+          >
+            <div className="picture-container-categories">
+              <img
+                src="https://server-store-p1t7.onrender.com/img/category-phones.png"
+                className="category-categories_section-picture"
+                alt="mobile phones"
+              />
             </div>
+            <h3 className="category-categories_section-title">
+              mobile phones
+            </h3>
+
+            <p className="category-categories_section-subtitle">95 models</p>
           </Link>
 
-          <Link to="/tablets">
-            <div
-              className="
-                category-categories_section
-                grid__item--desktop-9-16
-                grid__item--tablet-5-8
-                grid__item-1-4
-              "
-            >
-              <div className="picture-container-categories">
-                <img
-                  className="category-categories_section-picture"
-                  alt="mobile phones"
-                />
-              </div>
-              <h3 className="category-categories_section-title">
-                mobile phones
-              </h3>
-
-              <p className="category-categories_section-subtitle">95 models</p>
+          <Link
+            to="/tablets"
+            className="
+              category-categories_section
+              grid__item--desktop-9-16
+              grid__item--tablet-5-8
+              grid__item-1-4
+            "
+          >
+            <div className="picture-container-categories">
+              <img
+                src="https://server-store-p1t7.onrender.com/img/category-tablets.png"
+                className="category-categories_section-picture"
+                alt="tablets"
+              />
             </div>
+            <h3 className="category-categories_section-title">tablets</h3>
+
+            <p className="category-categories_section-subtitle">24 models</p>
           </Link>
 
-          <Link to="/accessories">
-            <div
-              className="
-                category-categories_section
-                grid__item--desktop-17-24
-                grid__item--tablet-9-12
-                grid__item-1-4
-              "
-            >
-              <div className="picture-container-categories">
-                <img
-                  className="category-categories_section-picture"
-                  alt="accessories"
-                />
-              </div>
-              <h3 className="category-categories_section-title">accessories</h3>
-
-              <p className="category-categories_section-subtitle">100 models</p>
+          <Link
+            to="/accessories"
+            className="
+              category-categories_section
+              grid__item--desktop-17-24
+              grid__item--tablet-9-12
+              grid__item-1-4
+            "
+          >
+            <div className="picture-container-categories">
+              <img
+                src="https://server-store-p1t7.onrender.com/img/category-accessories.png"
+                className="category-categories_section-picture"
+                alt="accessories"
+              />
             </div>
+            <h3 className="category-categories_section-title">accessories</h3>
+
+            <p className="category-categories_section-subtitle">100 models</p>
           </Link>
         </div>
       </div>
