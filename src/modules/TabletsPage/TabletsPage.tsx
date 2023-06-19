8c20aa671ff3020d7ebaf4e535991332900d6dc0
Denys Krysanov <denis.krisanov321@gmail.com>
2023-06-19 19:02:38 +0300

    added page stub for tablets and accessories

--- src/modules/TabletsPage/TabletsPage.tsx
+++ src/modules/TabletsPage/TabletsPage.tsx
@@ -1,7 +1,9 @@
-export const TabletsPage = () => {
-  return (
-    <div>
-      <h2>Tablets Page</h2>
-    </div>
-  );
-};
+import './TabletsPage.scss';
+
+export const TabletsPage = () => (
+  <div className="page">
+    <h1 className="page-title">
+      Coming soon...
+    </h1>
+  </div>
+);
