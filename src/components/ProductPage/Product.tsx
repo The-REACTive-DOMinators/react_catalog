e21873740de1cb67675c8f1033d846cb172a1194
Denys Krysanov <denis.krisanov321@gmail.com>
2023-06-21 19:50:15 +0300

    fixed bug with changing params

--- src/components/ProductPage/Product.tsx
+++ src/components/ProductPage/Product.tsx
@@ -102,9 +102,6 @@ export const Product: FC = () => {
     setSelectedCapacity(getSelectedCapacity());
   }, [phoneId, selectedColor, selectedCapacity]);
 
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  }, [phoneId]);
-
   return (
     <div className="global-container">
       <BreadCrumbs />
